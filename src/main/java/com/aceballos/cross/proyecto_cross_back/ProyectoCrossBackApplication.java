b2e9d8345763a1d29e67696c0179740e8c3030a0
Agustín Ceballos <aceballos1923@gmail.com>
2025-04-27 20:35:24 -0300

    initial commit

---
+++ src/main/java/com/aceballos/cross/proyecto_cross_back/ProyectoCrossBackApplication.java
@@ -0,0 +1,13 @@
+package com.aceballos.cross.proyecto_cross_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoCrossBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoCrossBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aceballos/cross/proyecto_cross_back/ProyectoCrossBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aceballos.cross.proyecto_cross_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoCrossBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
