774b0a00f4cb5b990c1ba6da8d20a195795d2479
Agustín Ceballos <aceballos1923@gmail.com>
2025-04-27 21:11:21 -0300

    feat: agrego repositorios

---
+++ src/main/java/com/aceballos/cross/proyecto_cross_back/repositories/IGrupoMuscularRepository.java
@@ -0,0 +1,9 @@
+package com.aceballos.cross.proyecto_cross_back.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.aceballos.cross.proyecto_cross_back.entities.GrupoMuscular;
+
+public interface IGrupoMuscularRepository extends JpaRepository<GrupoMuscular, Long> {
+
+}
