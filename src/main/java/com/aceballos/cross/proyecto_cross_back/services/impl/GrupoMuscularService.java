8d6e1c46ff891bc403cff4d1b8d8bdf631137e45
Agustín Ceballos <aceballos1923@gmail.com>
2025-04-29 00:59:28 -0300

    feat: completo el service
    Completo la logica del service de Grupo Muscular para poder realizar el controller completo

--- src/main/java/com/aceballos/cross/proyecto_cross_back/services/impl/GrupoMuscularService.java
+++ src/main/java/com/aceballos/cross/proyecto_cross_back/services/impl/GrupoMuscularService.java
@@ -1,13 +1,18 @@
 package com.aceballos.cross.proyecto_cross_back.services.impl;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.aceballos.cross.proyecto_cross_back.entities.GrupoMuscular;
+import com.aceballos.cross.proyecto_cross_back.exceptions.NoEncontradoException;
+import com.aceballos.cross.proyecto_cross_back.exceptions.YaExisteException;
 import com.aceballos.cross.proyecto_cross_back.repositories.IGrupoMuscularRepository;
 import com.aceballos.cross.proyecto_cross_back.services.IGrupoMuscularService;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class GrupoMuscularService implements IGrupoMuscularService{
@@ -22,26 +27,52 @@ public class GrupoMuscularService implements IGrupoMuscularService{
 
     @Override
     public GrupoMuscular traerGrupoMuscular(Long idGrupoMuscular) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'traerGrupoMuscular'");
+        Optional<GrupoMuscular> grupoMuscularOptional = grupoMuscularRepository.findById(idGrupoMuscular);
+
+        if(grupoMuscularOptional.isEmpty()) {
+            throw new NoEncontradoException(idGrupoMuscular);
+        }
+
+        return grupoMuscularOptional.get();
     }
 
     @Override
+    @Transactional
     public GrupoMuscular crearGrupoMuscular(GrupoMuscular grupoMuscular) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'crearGrupoMuscular'");
+        grupoMuscular.setNombre(grupoMuscular.getNombre().trim());
+
+        if(grupoMuscularRepository.existsByNombre(grupoMuscular.getNombre())) {
+            throw new YaExisteException(grupoMuscular.getNombre());
+        }
+
+        return grupoMuscularRepository.save(grupoMuscular);
     }
 
     @Override
     public GrupoMuscular modificarGrupoMuscular(GrupoMuscular grupoMuscular) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'modificarGrupoMuscular'");
+        Optional<GrupoMuscular> grupoMuscularOptional = grupoMuscularRepository.findById(grupoMuscular.getIdGrupoMuscular());
+
+        if(!grupoMuscularOptional.isPresent()) {
+            throw new NoEncontradoException(grupoMuscular.getNombre());
+        }
+
+        GrupoMuscular grupoMuscularModificar = grupoMuscularOptional.orElseThrow();
+        grupoMuscularModificar.setNombre(grupoMuscular.getNombre());
+
+        return grupoMuscularRepository.save(grupoMuscularModificar);
     }
 
     @Override
     public GrupoMuscular borrarGrupoMuscular(Long idGrupoMuscular) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'borrarGrupoMuscular'");
+        Optional<GrupoMuscular> grupoMuscularOptional = grupoMuscularRepository.findById(idGrupoMuscular);
+
+        if(!grupoMuscularOptional.isPresent()) {
+            throw new NoEncontradoException(idGrupoMuscular);
+        }
+
+        grupoMuscularRepository.deleteById(idGrupoMuscular);
+
+        return grupoMuscularOptional.get();
     }
 
 }
