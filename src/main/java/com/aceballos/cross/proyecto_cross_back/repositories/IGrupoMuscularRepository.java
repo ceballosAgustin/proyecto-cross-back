e78d38285c1c0ec7dafbfb5cbb3a65e44146449f
Agustín Ceballos <aceballos1923@gmail.com>
2025-04-29 00:58:34 -0300

    update: agrego logica en repositorio

--- src/main/java/com/aceballos/cross/proyecto_cross_back/repositories/IGrupoMuscularRepository.java
+++ src/main/java/com/aceballos/cross/proyecto_cross_back/repositories/IGrupoMuscularRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.aceballos.cross.proyecto_cross_back.entities.GrupoMuscular;
 
 public interface IGrupoMuscularRepository extends JpaRepository<GrupoMuscular, Long> {
-
+    
+    public boolean existsByNombre(String nombre);
 }
