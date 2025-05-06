d6d6b1e00a196c67a840f0f346c6e1ff197f6ae6
Agustín Ceballos <aceballos1923@gmail.com>
2025-05-06 12:26:59 -0300

    feat: repositorio de niveles de dificultad

---
+++ src/main/java/com/aceballos/cross/proyecto_cross_back/repositories/INivelDificultadRepository.java
@@ -0,0 +1,12 @@
+package com.aceballos.cross.proyecto_cross_back.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.aceballos.cross.proyecto_cross_back.entities.NivelDificultad;
+
+public interface INivelDificultadRepository extends JpaRepository<NivelDificultad, Long> {
+    
+    public boolean existsByNombre(String nombre);
+    
+    public boolean existsByNivel(Integer nivel);
+} 
